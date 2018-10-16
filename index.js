4052af4756cb38130e92010571a804efe884ff08
rkterungwa16 <richard@ashnelsonpartners.com>
2018-10-16 13:00:07 +0100

    Implement workers for the application

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Server = require('./lib/server')
+const Workers = require('./lib/workers')
+
+const workers = new Workers()
 const server = new Server()
 
 const app = {}
 
 app.init = () => {
   server.init()
+
+  workers.init()
 }
 
 app.init()
